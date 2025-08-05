a1a3738471ea40fbd459635fa3e9f427a5a785e0
Miguel Mota <mota.m.miguel@gmail.com>
2025-08-05 16:02:09 +0100

    Add Constants contract to keep track of all user-specific settings

---
+++ ethernaut/script/Constants.s.sol
@@ -0,0 +1,12 @@
+// SDPX-License-Identifier: MIT
+
+pragma solidity ^0.6.2;
+
+
+contract Constants {
+    address public constant ATTACKER_ADDRESS = 0xCDc986e956f889b6046F500657625E523f06D5F0;
+
+    address payable public constant FALLBACK_ADDRESS = payable(0x3Dd001d1ca706137c2B51C7D2167Aba5CdeD65FD);
+    address payable public constant FALLOUT_ADDRESS = payable(0x3a026133cC2e1138B7534Bb900cC4eB097E58920);
+    address public constant COIN_FLIP_ADDRESS = 0xE447a60be1fcfB04a7D3b69F57463c61d6fD0c83;
+}
